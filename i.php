b9574a002ece68e0f0f8cac7ca7bd0d48a5419ab
Alexandervera22 <39286550+Alexandervera22@users.noreply.github.com>
2019-03-25 14:09:40 -0500

    Create i.php

---
+++ i.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Registro de datos </title>
+
+</head>
+
+<body>
+
+<form action="registrado.php" method=get name ="formulario">
+<br>
+Nombre <input type="text" name ="nombre"  size ="30"/>
+
+<br>
+Telefono <input type="text" name ="telefono"  size ="30"/>
+
+<br>
+<input type="submit" name ="botonguardar" value="Guardar"/>
+
+</form>
+
+</body>
+<footer>
+</footer>
+</html>
